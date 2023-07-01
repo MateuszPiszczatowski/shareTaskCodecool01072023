b76369a81d8464637e3d021f734c85d3a8e902df
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-07-01 15:45:51 +0200

    update2

--- src/components/BlogPoster/BlogPoster.jsx
+++ src/components/BlogPoster/BlogPoster.jsx
@@ -11,15 +11,26 @@ class BlogPoster extends Component {
     console.log(this.state.postText, this.state.counter);
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.counter < 100) {
+      window.alert("Text too short, type moar");
+      return;
+    }
+    window.alert("Text ready to send");
+  };
+
   render() {
     return (
-      <form>
+      <form
+        onSubmit={this.handleSubmit}
+        style={{ display: "flex", flexDirection: "column", width: "fit-content" }}>
         <label htmlFor="text1">
           <h3></h3>
           <p></p>
         </label>
         <textarea id="text1" onInput={this.onTextInput} />
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" disabled={this.state.counter < 100 ? true : false} />
       </form>
     );
   }
